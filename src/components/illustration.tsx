87046b4b9c60b25d889536a0ae5af3e26e64438e
shjie047 <shjie047@gmail.com>
2024-08-27 12:53:04 +0800

    fix: assets

--- src/components/illustration.tsx
+++ src/components/illustration.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Illustration = () => {
   return (
     <Image
-      src="/assets/illustration.svg"
+      src="/assets/illustrations.svg"
       width={601}
       height={515}
       alt="illustration"
